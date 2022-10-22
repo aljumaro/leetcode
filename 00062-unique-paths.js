f6e95915edfb3118cd1efba75fb17650080a4e25
Juanma <aljumaro@gmail.com>
2022-10-22 09:48:38 +0200

    62 Unique paths

--- 00062-unique-paths.js
+++ 00062-unique-paths.js
@@ -14,24 +14,19 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let matrix = new Array(m);
-    for (let k = 0; k < matrix.length; k++) {
-        matrix[k] = new Array(n).fill(-1);
+    let matrix = [[]];
+    for (let i = 0; i < n; i++) {
+        matrix[0].push(1);
     }
-    for (let i = 0; i < matrix.length; i++) {
-        matrix[i][0] = 1;
-    }
-    for (let i = 0; i < matrix[0].length; i++) {
-        matrix[0][i] = 1;
-    }
+    for (let i = 1; i < m; i++) {
+        matrix.push([1]);
 
-    for (let i = 1; i < matrix.length; i++) {
-        for (let j = 1; j < matrix[0].length; j++) {
+        for (let j = 1; j < n; j++) {
             matrix[i][j] = matrix[i-1][j] + matrix[i][j-1];
         }
     }
     return matrix[m-1][n-1];
 };
 
-let m = 4, n = 4;
+let m = 3, n = 3;
 console.log(uniquePaths(m, n));
